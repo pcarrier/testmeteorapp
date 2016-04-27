ae1c96d98f15fc0a053616f9d71f9c8899cf123f
Pierre Carrier <pierre@meteor.com>
2016-04-27 14:32:32 -0700

    oom

--- server/main.js
+++ server/main.js
@@ -1,1 +1,7 @@
-setTimeout(function(){process.exit(1);}, 1000);
+var leak=[];
+setInterval(function(){
+  var s = "", n = Math.random();
+  for (i = 0; i < 100000; i++) s = s + n;
+  leak.push(s);
+  console.log('hop');
+}, 1);
