c7f685df0fadfc1a52abce42c692636fbf1f84c5
Pierre Carrier <pierre@meteor.com>
2016-05-27 15:28:52 -0700

    wrap in Meteor.startup

--- server/main.js
+++ server/main.js
@@ -1,7 +1,9 @@
 var leak=[];
-setInterval(function(){
-  var s = "", n = Math.random();
-  for (i = 0; i < 100000; i++) s = s + n;
-  leak.push(s);
-  console.log('hop');
-}, 1);
+Meteor.startup(function(){
+  setInterval(function(){
+    var s = "", n = Math.random();
+    for (i = 0; i < 100000; i++) s = s + n;
+    leak.push(s);
+    console.log('hop');
+  }, 1);
+});
